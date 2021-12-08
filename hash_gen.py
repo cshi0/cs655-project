a843c344ac6676cac23f2e785dc5a276fe6d6d18
Chenming Shi <clevercshi@gmail.com>
2021-12-08 11:11:49 -0500

    added hash_gen script

---
+++ hash_gen.py
@@ -0,0 +1,20 @@
+import random
+import hashlib
+import requests
+
+chars = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)]
+
+strs = []
+
+for _ in range(10):
+  s = ""
+  for _ in range(5):
+    s += random.choice(chars)
+  strs += [s]
+
+print(strs)
+
+for s in strs:
+  h = hashlib.md5(s.encode())
+  print("curl -X POST http://localhost:8080/crackTask --content application/json --data '{\"Addr\": " +h.hexdigest()+ "}'")
+  requests.post('http://localhost:8080/crackTask', {"Addr": h.hexdigest()})
